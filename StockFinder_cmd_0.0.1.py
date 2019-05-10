9ddcf19e9ee5fc620bc97b9d4f3467c3fe8872de
phytonian <mailto.shirke@gmail.com>
2019-05-10 12:41:29 +0530

    Command line version of stock finder in excel
    Its a crude version to find the stock in excel and print result if found or not

---
+++ StockFinder_cmd_0.0.1.py
@@ -0,0 +1,23 @@
+from openpyxl import load_workbook # importing workbook module
+fPath=r"C:\Users\arvind\Desktop\StockTake.xlsx"  # Setting File path
+
+wb=load_workbook(fPath) #loading workbook in memory
+ws=wb.active # setting active worksheet
+
+found=False # intialize stock found status to false
+
+for i in range(0,5):
+    scanned=input("Please scan bar scanned : ") # scanning scanned 10 times
+
+    Add_col=ws["I"] # Column to be searched
+    
+    for x in range(len(Add_col)): # getting column range
+        if scanned!=Add_col[x].value: # comparing result
+            found=False
+        else:
+            print("Stock Found")
+            found=True
+            break
+    if found==False:
+        print("stock not found")
+    
